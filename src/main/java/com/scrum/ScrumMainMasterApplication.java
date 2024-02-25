2cd23ed7e3eb994327cf69dde29f3a56d049a0d5
Akhil Vudatha <akhilvsvs1999@gmail.com>
2024-02-26 01:29:19 +0530

    Initial Commit

---
+++ src/main/java/com/scrum/ScrumMainMasterApplication.java
@@ -0,0 +1,13 @@
+package com.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumMainMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumMainMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrum/ScrumMainMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumMainMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
